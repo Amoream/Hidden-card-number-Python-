ddd9cdf3eba64195bab805d01adcdf9b7edff308
Amoream <107493699+Amoream@users.noreply.github.com>
2023-06-05 18:30:20 +0200

    Add files via upload

---
+++ Python card__number.py
@@ -0,0 +1,23 @@
+"""
+Write a function in Python that accepts a credit card number. 
+It should return a string where all the characters are hidden with an asterisk except the last four. 
+For example, if the function gets sent “4444444444444444”, then it should return “4444”.
+"""
+
+def hide_card_number():
+
+    hidden_code = ""
+
+    #Retrieve the card number of the user
+    card_num = input('Enter your credit card number: ') #Retrieve the card number of the user
+
+    #Add the digits into the empty string as * and overwrite the last * by the digits of the card's numbers
+    for digit in card_num:
+        hidden_code += '*'
+    hidden_code += card_num[-4:]
+
+    return hidden_code
+
+
+hide_the_code = hide_card_number()
+print(hide_the_code)
